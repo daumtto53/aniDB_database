347f444fa316a5015650bcff9e03e2494b4da004
daumtto53 <daumtto53@gmail.com>
2024-07-24 01:21:18 +0900

    env: requirements.txt

--- selenium/mangaupdates/NovelInfo.py
+++ selenium/mangaupdates/NovelInfo.py
@@ -1,21 +1,21 @@
 class NovelInfo:
 
     def __init__(self):
+        self.title = None
         self.description = None
         self.type = None
-        # {title:"", relation:""}
-        self.related_series = { }
-        # {title:"", novel/manga:"", name_origin_country:""}
+        self.year = None
+        # [{title:"", relation:""}]
+        self.related_series = {}
+        # [{title:"", novel/manga:"", name_origin_country:""}, ...]
         self.associated_names = {}
         # {volumes:(int), complete/ongoing==true/false}
-        self.status_in_origin_country = True
+        self.status_in_origin_country = {}
         # [{season_starts_at:INT, volume_starts_at:INT, season_ends_at:INT, volume_ends_at:INT}, ...]
-        self.anime_start_and_end = []
-        self.genre=[]
         self.authors = []
         self.artists = []
-        self.year = None
-        self.original_publisher = None
-        self.serialized_in = None
-
+        self.original_publisher = []
+        self.serialized_in = []
+        self.genre=[]
+        self.anime_start_and_end = []
 
